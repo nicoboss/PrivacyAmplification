a7029cd32ed4353f5188d70b8f666b753b165f8b
Nico Bosshard <nico.bosshard@stud.hslu.ch>
2020-08-12 23:59:51 +0200

    Copy over private key as most will SSH to the container from a different machine then the docker host

--- run_docker.sh
+++ run_docker.sh
@@ -3,6 +3,7 @@ if [ ! -f ~/.ssh/PrivacyAmplificationDocker.pub ]; then
 ssh-keygen -t rsa -b 4096 -f ~/.ssh/PrivacyAmplificationDocker -N "" -C "PrivacyAmplificationDocker"
 ssh-add ~/.ssh/PrivacyAmplificationDocker
 fi
+cp -a ~/.ssh/PrivacyAmplificationDocker PrivacyAmplificationDocker.key
 cp -a ~/.ssh/PrivacyAmplificationDocker.pub PrivacyAmplificationDocker.pub
 docker build -t privacyamplification -f Dockerfile .
 rm -f PrivacyAmplificationDocker.pub
