2ad9427729b07966c58b658dd2f9515ed2ac5af6
Nico Bosshard <nico.bosshard@stud.hslu.ch>
2020-08-13 11:33:58 +0200

    The SSH Private Key now getting automatically converted from the OpenSSH Private Key format into the PuTTY Private Key format

--- run_docker.sh
+++ run_docker.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 if [ ! -f ~/.ssh/PrivacyAmplificationDocker.pub ]; then
-ssh-keygen -t rsa -b 4096 -f ~/.ssh/PrivacyAmplificationDocker -N "" -C "PrivacyAmplificationDocker"
-ssh-add ~/.ssh/PrivacyAmplificationDocker
+  ssh-keygen -t rsa -b 4096 -f ~/.ssh/PrivacyAmplificationDocker -N "" -C "PrivacyAmplificationDocker"
+  ssh-add ~/.ssh/PrivacyAmplificationDocker
+  cp -a ~/.ssh/PrivacyAmplificationDocker PrivacyAmplificationDocker.key
+  cat PrivacyAmplificationDocker.key | docker run -i cubeearth/puttygen@sha256:42107acb9d1d4a7ce0d6b5d89e20f55c00dcfb76537aa21c5eef621c6905abff > PrivacyAmplificationDocker.ppk
 fi
-cp -a ~/.ssh/PrivacyAmplificationDocker PrivacyAmplificationDocker.key
 cp -a ~/.ssh/PrivacyAmplificationDocker.pub PrivacyAmplificationDocker.pub
 docker build -t privacyamplification -f Dockerfile .
 rm -f PrivacyAmplificationDocker.pub
