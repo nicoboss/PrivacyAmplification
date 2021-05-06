134e682d43ff98475c517b94ae26060aa2a9b3fa
nicoboss <nico@bosshome.ch>
2021-05-06 18:59:26 +0200

    Integrated ReceiveAmpOutExample into LargeBlocksizeExample with clean multithreading

--- examples/LargeBlocksizeExample/main.cpp
+++ examples/LargeBlocksizeExample/main.cpp
@@ -7,6 +7,7 @@
 #include <time.h>
 #include <thread>
 #include <mutex>
+#include <bitset>
 #include <atomic>
 #include <zmq.h>
 using namespace std;
@@ -28,12 +29,16 @@ const char* address_seed_server = "tcp://127.0.0.1:45555";
 /*Address where the key server should bind to.*/
 const char* address_key_server = "tcp://127.0.0.1:47777";
 
+/*Address of the Privacy Amplification Server*/
+const char* privacyAmplificationServer_address = "tcp://127.0.0.1:48888";
+
 /*Privacy Amplification input size in bits
   Has to be 2^x and 2^27 is the maximum
   Needs to match with the one specified in other components*/
 #define factor 27
 #define pwrtwo(x) (1 << (x))
 #define sample_size pwrtwo(factor)
+#define min(a,b) (((a) < (b)) ? (a) : (b))
 
 constexpr uint32_t vertical_len = sample_size / 4 + sample_size / 8;
 constexpr uint32_t horizontal_len = sample_size / 2 + sample_size / 8;
@@ -46,6 +51,8 @@ constexpr uint32_t desired_len = vertical_len + horizontal_len;
 unsigned int* toeplitz_seed = (unsigned int*)malloc(desired_block * sizeof(uint32_t));
 int32_t reuseSeedAmount = -1;
 unsigned int* key_data = new unsigned int[key_blocks];
+constexpr uint32_t vertical_bytes = vertical_len / 8;
+unsigned char* ampOutInData = (unsigned char*)malloc(vertical_bytes);
 
 atomic<int> seedServerReady = 1;
 atomic<int> keyServerReady = 1;
@@ -241,6 +248,50 @@ void keyProvider()
 	}
 }
 
+/// @brief Receives data from the Privacy Amplification Sevrer
+/// Contains all the comunication and error handling required
+/// to receive the privacy amplification results from the Privacy Amplification Server.
+/// Currently it prints the first 4 bytes of the result on the screen.
+/// Note: In a real environment this code should be integrated in whatever
+/// tool makes use of the Privacy Amplification result.
+int receiveAmpOut()
+{
+	int32_t rc;
+	time_t currentTime;
+	void* context = zmq_ctx_new();
+	int timeout = 1000;
+
+	reconnect:;
+	void* ampOutIn_socket = zmq_socket(context, ZMQ_REQ);
+	zmq_setsockopt(ampOutIn_socket, ZMQ_RCVTIMEO, &timeout, sizeof(int));
+
+	cout << "Waiting for PrivacyAmplification Server..." << endl;
+	zmq_connect(ampOutIn_socket, privacyAmplificationServer_address);
+
+	while (true) {
+		zmq_send(ampOutIn_socket, "SYN", 3, 0);
+		rc = zmq_recv(ampOutIn_socket, ampOutInData, vertical_bytes, 0);
+		if (rc != vertical_bytes) {
+			cout << "Error receiving data from PrivacyAmplification Server!" << endl;
+			cout << "Expected " << vertical_bytes << " bytes but received " << rc << " bytes! Retrying..." << endl;
+			zmq_close(ampOutIn_socket);
+			goto reconnect;
+		}
+
+		time(&currentTime);
+		cout << put_time(localtime(&currentTime), "%F %T") << " Key Block recived" << endl;
+
+		for (size_t i = 0; i < min(vertical_bytes, 4); ++i)
+		{
+			printf("0x%02X: %s\n", ampOutInData[i], bitset<8>(ampOutInData[i]).to_string().c_str());
+		}
+	}
+
+	zmq_close(ampOutIn_socket);
+	zmq_ctx_destroy(ampOutIn_socket);
+	return 0;
+}
+
 
 /// @brief The main function cordinates all servers
 /// Ensures that all servers have serverd thair data to its clients 
@@ -255,6 +306,8 @@ int main(int argc, char* argv[])
 	threadSendSeed.detach();
 	thread threadSendKey(sendKey);
 	threadSendKey.detach();
+	thread threadReceiveAmpOut(receiveAmpOut);
+	threadReceiveAmpOut.detach();
 	while(true) {
 		std::this_thread::sleep_for(10s);
 		//println("[Stat] Still alive");
