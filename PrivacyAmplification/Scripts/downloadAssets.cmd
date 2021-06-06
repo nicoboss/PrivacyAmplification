ba9e1caf99bc14e8bcc3a18a9305cc7c5edc36be
nicoboss <nico@bosshome.ch>
2021-06-06 11:02:20 +0200

    downloadAssets.cmd: Fixed the AzureCLI and azure-devops installation

--- PrivacyAmplification/Scripts/downloadAssets.cmd
+++ PrivacyAmplification/Scripts/downloadAssets.cmd
@@ -1,8 +1,18 @@
-WHERE az
-IF %ERRORLEVEL% NEQ 0 powershell -command "Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; rm .\AzureCLI.msi"; az extension add --name azure-devops
+@echo off
+echo Checking for AzureCLI and azure-devops...
+WHERE az > nul
+IF %ERRORLEVEL% NEQ 0 (
+  powershell -command "Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi"
+  powershell -command "Get-Location; Start-Process msiexec.exe -verb runAs -Wait -ArgumentList '/I "%cd%"\AzureCLI.msi /quiet'"
+  del /f .\AzureCLI.msi
+  call az extension add --name azure-devops
+)
+set PATH=%PATH%;"C:\Program Files (x86)\Microsoft SDKs\Azure\CLI2\wbin\"
 
+echo Login to Azure DevOps...
 echo 73fvfjbljqa7h7l5asob5rxbggay5lgva6osjinomk7hwlbosifa | az devops login --organization https://dev.azure.com/nicoboss/
 
+echo Downloading assets from Azure DevOps...
 call az artifacts universal download ^
   --organization "https://dev.azure.com/nicoboss/" ^
   --project "d47efac3-15b4-4aae-9e4d-6a4c0a44a420" ^
